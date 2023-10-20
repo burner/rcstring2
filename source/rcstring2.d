c5518d5e9aba51dadd873790d8618656ba5c18e4
Robert burner Schadek <rburners@gmail.com>
2023-10-20 22:08:08 +0200

    ~ and ~= seem to work

--- source/rcstring2.d
+++ source/rcstring2.d
@@ -1,111 +1,221 @@
 module rcstring2;
 
+import std.stdio;
+
 import core.memory : GC;
 
-struct PayloadHeap {
-	char* ptr;
+private struct PayloadHeap {
 	long refCnt;
-	size_t capacity;
+	char* ptr;
 }
 
 enum SmallStringMaxSize = 56;
 
-union Payload {
+private union Payload {
 	char[SmallStringMaxSize] small;
-	PayloadHeap ptr;
+	PayloadHeap* ptr;
 }
 
 public struct String {
 	Payload impl;
-	uint length;
+	private uint theLength;
+
+	@property uint length() const @safe {
+		return this.theLength;
+	}
 
 	this(string input) {
-		this.assign(input);
-	}
-
-	this(typeof(this) n) {
-		this.assign(n);
-	}
-
-	this(this) {
-		if(this.length > SmallStringMaxSize) {
-			this.impl.ptr.refCnt++;
+		if(input.length > SmallStringMaxSize) {
+			this.allocate(input.length);
+			this.impl.ptr.ptr[0 .. input.length] = cast(char[])input[];
+		} else {
+			this.impl.small[0 .. input.length] = input;
 		}
+		this.theLength = cast(uint)input.length;
+	}
+
+	this(ref return scope String n) {
+		if(n.length > SmallStringMaxSize) {
+			this.impl.ptr = n.impl.ptr;
+			this.impl.ptr.refCnt++;
+			writeln(__LINE__);
+		} else {
+			this.impl.small = n.impl.small;
+		}
+
+		this.theLength = n.length;
 	}
 
 	~this() @trusted {
-		if(this.length > SmallStringMaxSize) {
+		if(this.theLength > SmallStringMaxSize) {
 			this.impl.ptr.refCnt--;
 			if(this.impl.ptr.refCnt <= 0) {
 				GC.free(this.impl.ptr.ptr);
+				GC.free(this.impl.ptr);
 			}
 		}
 	}
 
 	@property bool empty() const nothrow {
-		return this.length == 0;
+		return this.theLength == 0;
 	}
 
-	typeof(this) opBinary(string op,S)(S other) @trusted
+	version(unittest) {
+		PayloadHeap* getPayload() {
+			return this.theLength < SmallStringMaxSize
+				? null
+				: this.impl.ptr;
+		}
+	}
+
+	string getData() @system {
+		return cast(string)(this.theLength < SmallStringMaxSize
+			? this.impl.small[0 .. this.theLength]
+			: this.impl.ptr.ptr[0 .. this.theLength]
+		);
+	}
+
+	String opBinary(string op,S)(S other) @trusted
 			if(op == "~")
 	{
 		String ret;
 
+		const newLen = this.theLength + other.length;
+		ret.theLength = cast(uint)newLen;
+
 		static if(is(S == string)) {
-			const newLen = this.length + other.length;
 			if(newLen < SmallStringMaxSize) {
-				ret.impl.small[0 .. this.length] = this.impl.small;
-				ret.impl.small[this.length .. newLen] = other;
+				ret.impl.small[0 .. this.theLength] = cast(char[])this.getData();
+				ret.impl.small[this.theLength .. newLen] = other;
 			} else {
 				ret.allocate(newLen);	
+				ret.impl.ptr.ptr[0 .. this.theLength] = cast(char[])this.getData();
+				ret.impl.ptr.ptr[this.theLength .. newLen] = cast(char[])other;
 			}
 		} else static if(is(S == String)) {
-		}
-	}
-
-	private void assign(typeof(this) n) @trusted {
-		if(this.length > SmallStringMaxSize) {
-			this.impl.ptr.refCnt--;
-			if(this.impl.ptr.refCnt <= 0) {
-				GC.free(this.impl.ptr.ptr);
+			if(newLen < SmallStringMaxSize) {
+				ret.impl.small[0 .. this.theLength] = cast(char[])this.getData();
+				ret.impl.small[this.theLength .. newLen] = cast(char[])other.getData();
+			} else {
+				ret.allocate(newLen);	
+				ret.impl.ptr.ptr[0 .. this.theLength] = cast(char[])this.impl.getData();
+				ret.impl.ptr.ptr[this.theLength .. newLen] = cast(char[])other.getData();
 			}
 		}
 
-		if(n.length > SmallStringMaxSize) {
-			this.impl.ptr = n.impl.ptr;
-			this.impl.ptr.refCnt++;
-		} else {
-			this.impl.small = n.impl.small;
-		}
-
-		this.length = n.length;
+		return ret;
 	}
 
-	private void assign(string input) @trusted {
-		if(input.length > SmallStringMaxSize) {
-			this.allocate(input.length);
-			this.impl.ptr.ptr[0 .. input.length] = input;
-		} else {
-			this.impl.small[0 .. input.length] = input;
+	void opOpAssign(string op,S)(S other) @trusted
+			if(op == "~")
+	{
+		const newLen = this.theLength + other.length;
+
+		if(newLen > SmallStringMaxSize && this.theLength < SmallStringMaxSize) {
+			char[SmallStringMaxSize] copy = this.impl.small[];
+			this.allocate(newLen);
+			this.impl.ptr.ptr[0 .. this.theLength] = copy[0 .. this.theLength];
+		} else if(newLen > SmallStringMaxSize && this.theLength > SmallStringMaxSize) {
+			this.realloc(newLen);
 		}
-		this.length = cast(uint)input.length;
+
+		static if(is(S == string)) {
+			if(newLen < SmallStringMaxSize) {
+				this.impl.small[this.theLength .. newLen] = other;
+			} else {
+				this.impl.ptr.ptr[this.theLength .. newLen] = cast(char[])other;
+			}
+		} else static if(is(S == String)) {
+			if(newLen < SmallStringMaxSize) {
+				this.impl.small[this.theLength .. newLen] = cast(char[])other.getData();
+			} else {
+				this.impl.ptr.ptr[this.theLength .. newLen] = cast(char[])other.getData();
+			}
+		}
+
+		this.theLength = cast(uint)newLen;
+	}
+
+	private void realloc(const size_t newLen) @trusted {
+		if(this.theLength < SmallStringMaxSize) {
+			this.allocate(newLen);
+		} else {
+			this.impl.ptr.ptr = cast(char*)GC.realloc(this.impl.ptr.ptr, newLen);
+		}
 	}
 
 	private void allocate(const size_t newLen) @trusted {
-		if(this.length <= SmallStringMaxSize) {
-			this.impl.ptr = PayloadHeap.init;
-		}
-		if(newLen > SmallStringMaxSize) {
-			this.impl.ptr.ptr = cast(char*)GC.realloc(this.impl.ptr.ptr, newLen);
-			this.impl.ptr.refCnt = 1;
-			this.impl.ptr.capacity = newLen;
-		}
+		this.impl.ptr = cast(PayloadHeap*)GC.malloc(PayloadHeap.sizeof);
+		this.impl.ptr.ptr = cast(char*)GC.malloc(newLen);
+		this.impl.ptr.refCnt = 1;
 	}
 }
 
 unittest {
 	static assert(String.sizeof == 64);
+}
 
+unittest {
 	auto s = String("Hello World");
+}
+
+unittest {
 	auto t = String("Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World");
+}
+
+unittest {
+	auto r = String("Hello") ~ " World";
+	assert(r.getData() == "Hello World", r.getData());
+
+	String g;
+	g ~= "Hello World Hello World Hello World Hello World";
+	g ~= "Hello World Hello World Hello World Hello World";
+	g ~= "Hello World Hello World Hello World Hello World";
+}
+
+unittest {
+	String g;
+	g ~= "Hello World Hello World Hello World Hello World";
+	String l = g;
+
+	g ~= "Hello World Hello World Hello World Hello World";
+	g ~= "Hello World Hello World Hello World Hello World";
+
+	String h = g;
+	String j = h;
+}
+
+unittest {
+	String a;
+	a = a;
+	a = String("Hello World");
+}
+
+unittest {
+	String fun() {
+		auto t = String("Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World");
+		assert(t.getPayload.refCnt == 1);
+		return t;
+	}
+
+	String s;
+	assert(s.getPayload is null);
+	s = fun();
+	assert(s.getPayload.refCnt == 1);
+}
+
+unittest {
+	void bar(String s) {
+		assert(s.getPayload.refCnt == 3);
+	}
+
+	void fun(String s) {
+		assert(s.getPayload !is null);
+		assert(s.getPayload.refCnt == 2);
+		bar(s);
+	}
+
+	auto t = String("Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World Hello World");
+
+	fun(t);
 }
